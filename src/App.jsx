a333049cb20eaf0ccd7a6fac184c51e261a5c830
Kunal verma <kunal33grd@gmail.com>
2025-09-22 16:24:36 +0530

    Starting the project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import Footer from './components/Footer'
+function App() {
+ 
+
+  return (
+    <>
+      <div className="header h-[45px] border-2 border-amber-700 ">
+
+      </div>
+        <Footer/>
+    </>
+  )
+}
+
+export default App
